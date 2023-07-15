baa926d7f70b12c111c96353a5be89037ef13f4a
RuslanMirasov <info@mirasov.dev>
2023-07-15 22:41:26 +0200

    Video task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playerSaveTime = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+const playerCurrentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(playerCurrentTime || 0);
+
+player.on('timeupdate', playerSaveTime);
